f6def59d829be32cd98a9bb95f182549048decec
mquangpham575 <pmqminhquang@gmail.com>
2026-04-01 20:44:50 +0700

    Revert "feat: lab3"
    This reverts commit d07d74170693db25a1ace3391033baf354c927da.

--- lab3/lab3_1/app/src/main/java/com/example/lab3_1/Contact.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lab3_1
-
-data class Contact(
-    val id: Int = 0,
-    val name: String = "",
-    val phone: String = ""
-)
--- lab3/lab3_2/app/src/main/java/com/example/lab3_2/Student.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lab3_2
-
-data class Student(
-    val id: Int = 0,
-    val name: String = "",
-    val mssv: String = "",
-    val lop: String = "",
-    val diem: Float = 0f
-)
